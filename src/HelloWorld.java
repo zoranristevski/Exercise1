db030c13efbfdd4f1e8292e9404a9ca8c0adec44
DESKTOP-L0632E6\Zoran <zoran.ristevski@tum.de>
2017-11-04 23:19:15 +0100

    Adapted HelloWorld for new HelloUser class.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello ASE2017 - How are you? :)");
+		HelloUser user = new HelloUser();
+		InputStreamReader isr = new InputStreamReader(System.in);
+	    BufferedReader br = new BufferedReader(isr);
+	    
+	    
+	    System.out.println("Hello ASE2017 - How are you now? :)");
+	    System.out.print("Please enter your name: ");
+	    try{
+	    	user.setUsername(br.readLine());
+	    }
+	    catch(IOException e){
+			e.printStackTrace();
+		}
+	    user.greetUser();
 	}
 
 }
